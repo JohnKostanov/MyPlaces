f5e58e59f5282e29a377584f571b093f76460de4
JohnKostanov <kostanovd@icloud.com>
2019-06-18 20:38:42 +0200

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by  Джон Костанов on 18/06/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
