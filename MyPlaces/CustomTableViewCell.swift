eb5497d66085e3093d9f77bd5647617dc666e808
JohnKostanov <kostanovd@icloud.com>
2019-06-19 13:25:49 +0200

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by  Джон Костанов on 19/06/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
